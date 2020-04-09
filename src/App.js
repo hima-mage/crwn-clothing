769201387d637565a2c034e8c40b93e7e23b8052
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-09 11:45:44 +0200

    adding header component and link logo to home page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {Route, Switch } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
- 
+import Header from './component/header/header.component'
 
 
 function App() {
   return (
     <div className="App">
         {/* <HomePage/> */}
+        {/* out of switch to alaways present and render */}
+        <Header />
         <Switch>
         <Route  exact path='/' component={HomePage} /> 
         <Route   path='/shop' component={ShopPage} /> 
