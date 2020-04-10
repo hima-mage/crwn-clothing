cce5eca9d673c1550d8a2462431c42bc0d4862c5
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-10 07:20:43 +0200

    creating sign page with sigin & Frominput & customButton components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react'; 
-import {Route, Switch } from 'react-router-dom'
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import Header from './component/header/header.component'
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./component/header/header.component";
+import signInAndUpPage from "./pages/sign-in-and-up/sign-in-&-up.component";
 
 function App() {
   return (
     <div className="App">
-        {/* <HomePage/> */}
-        {/* out of switch to alaways present and render */}
-        <Header />
-        <Switch>
-        <Route  exact path='/' component={HomePage} /> 
-        <Route   path='/shop' component={ShopPage} /> 
-        </Switch>
+      {/* <HomePage/> */}
+      {/* out of switch to alaways present and render */}
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={signInAndUpPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/sign-in-and-up/sign-in-&-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react' 
+import SignIn from '../../component/sign-in/sign-in.component'
+ 
+import './sign-in-&-up.styles.scss'
+
+
+const signInAndUpPage =() => (
+    <div className="sign-in-and-up">
+       <SignIn />
+    </div>
+)
+ 
+export default signInAndUpPage 
