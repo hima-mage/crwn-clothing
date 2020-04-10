6eb75859d034497bbace3e4965959f5b27acaa32
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-10 09:55:42 +0200

    in App.js i made  consisteny checking auth and make sure to kill the connection once the app in umount to prevent any memory leaking

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./component/header/header.component";
 import Sign from "./pages/sign/sign.component";
+// auth state will live in app to pass into any component needed
+import {auth } from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div className="App"> 
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/sign" component={Sign} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubcribeFromAuth = null; 
+
+  componentDidMount() {
+    // return method which will be used to close channel 
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth()
+  }
+
+
+  render() {
+    return (
+      <div className="App"> 
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/sign" component={Sign} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
