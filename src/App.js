50d1979b7060e469e58aff59cc6c3313cf3650b0
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-12 17:45:48 +0200

    redirect user from sign if he authenticate and set the types of the action in sigle file to avoid any accident

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -43,15 +43,20 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/sign" component={Sign} />
+          <Route exact path="/sign" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<Sign />)} />
         </Switch>
       </div>
     );
   }
 }
  
+//  get the user state to controll access to sign page
+const mapStateToProps = ({user }) => ({
+    currentUser : user.currentUser
+})
+// controll user auth state change
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
