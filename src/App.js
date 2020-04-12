bca7b2ee7b2a255571e778bbd5788402bbd5f7cc
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-12 08:18:22 +0200

    mapDispatchToProps set in app to control user auth state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
+import { connect } from "react-redux";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./component/header/header.component";
 import Sign from "./pages/sign/sign.component";
 // auth state will live in app to pass into any component needed
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
+
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null,
-    };
-  }
-
   unsubcribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     // return method which will be used to close channel
     this.unsubcribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot((snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(), // all data of this doc
-            },
-          });
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
+          }); // end of setCurrentUser
         });
       } else {
-        this.setState({
-          currentUser: userAuth,
-        });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -47,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header   />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -57,5 +49,9 @@ class App extends Component {
     );
   }
 }
+ 
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
-export default App;
+export default connect(null, mapDispatchToProps)(App);
