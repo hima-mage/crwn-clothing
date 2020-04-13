f4f60f2e0f8484307403ba0aa2b22c977c92b06a
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-13 09:32:54 +0200

    modify cart state to have items add to it when click in add to cart button in each product

--- src/component/collection-item/collection-item.component.jsx
+++ src/component/collection-item/collection-item.component.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import CustomButton from '../custom-button/custom-button.component'
-import './collection-item.styles.scss'
+import React from "react";
+import { connect } from "react-redux";
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
+import "./collection-item.styles.scss";
 
-const ColectionItem =  ({id, name, price ,  imageUrl }) => (
+const ColectionItem = ({ item, addItem }) => {
+  const {  name, price, imageUrl } = item;
+  return (
     <div className="collection-item">
-        <div className="image" style={{ backgroundImage:`url(${imageUrl})`}} /> 
-            <div className="collection-footer"> 
-                <span className='name' >{name}</span>
-                <span className='price' >{price}</span>
-            </div>
-            <CustomButton inverted>Add To Cart</CustomButton>
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>Add To Cart</CustomButton>
     </div>
-)
-
-export default ColectionItem
+  );
+};
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+export default connect(null, mapDispatchToProps)(ColectionItem);
