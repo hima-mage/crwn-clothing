fafb4b68d44175ebf00da52798ac48d234794a7b
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-17 16:41:50 +0200

    get items and title of subpage [category]  into collection component , and add style that component

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import {connect} from 'react-redux'
-import {selectCollection} from '../../redux/shop/shop.selectors'
-import CollectionItem from '../../component/collection-item/collection-item.component'
-import "./collection.styles.scss"
+import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../component/collection-item/collection-item.component";
+import "./collection.styles.scss";
 
 const ColectionPage = ({ collection }) => {
-    console.log(collection)
-   return ( <div className="collection-page">
-         <h2>collection </h2>
-    </div>)
-}
-
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <h2 className="title">{title} </h2>
+      <div className="items">
+          {
+              items.map(item => 
+                 <CollectionItem key={item.id} item={item} />
+                )
+          }
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
-    collection : selectCollection(ownProps.match.params.collectionId)(state)
-})
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
-export default connect(mapStateToProps) (ColectionPage)
+export default connect(mapStateToProps)(ColectionPage);
