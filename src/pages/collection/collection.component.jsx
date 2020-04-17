0ab1fea6893789d6992004e81de87097bbff8b6d
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-17 15:31:03 +0200

    geting collection based in url using selector , and mao through my order object of collection title and id

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {selectCollection} from '../../redux/shop/shop.selectors'
 import CollectionItem from '../../component/collection-item/collection-item.component'
 import "./collection.styles.scss"
 
-const ColectionPage = ({match }) => (
-    <div className="collection">
+const ColectionPage = ({ collection }) => {
+    console.log(collection)
+   return ( <div className="collection-page">
          <h2>collection </h2>
-    </div>
-)
+    </div>)
+}
 
 
-export default ColectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection : selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps) (ColectionPage)
