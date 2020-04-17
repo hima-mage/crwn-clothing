3e5e1e133fa85c15bba2b0db850d44467cec5691
isheikh-dev <isheikh-dev@gmail.com>
2020-04-17 14:30:14 +0200

    separate shop page to create collection-overview component which will display only the collection category items when select it's route

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import CollectionPreview from "../../component/preview-collection/collection-preview.component";
-import { selectCollections } from "../../redux/shop/shop.selectors";
-const ShopPage = ({ collections }) => (
+
+import CollectionOverview from "../../component/collection-overview/collection-overview.component";
+const ShopPage = () => (
   <div>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
-export default connect(mapStateToProps) (ShopPage);
+export default ShopPage;
