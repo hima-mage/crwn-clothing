5d662aed6e00758daa758ae7687fb40e3d2c6d57
isheikh-dev <isheikh-dev@gmail.com>
2020-04-17 13:36:31 +0200

    put the shop collection into redux and use selectors with it , and call it in shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,17 @@
-import React, { Component } from "react"; 
-import CollectionPreview from '../../component/preview-collection/collection-preview.component'
-import SHOP_DATA from './shop.data'
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CollectionPreview from "../../component/preview-collection/collection-preview.component";
+import { selectCollections } from "../../redux/shop/shop.selectors";
+const ShopPage = ({ collections }) => (
+  <div>
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-export default class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections:SHOP_DATA
-    };
-  }
-  render() {
-    const {collections } = this.state
-    return (<div>
-        {
-            collections.map(({id , ...otherCollectionProps}) =>(
-                <CollectionPreview key={id} {...otherCollectionProps} />                
-            ))
-
-        }
-    </div>)
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+})
+export default connect(mapStateToProps) (ShopPage);
