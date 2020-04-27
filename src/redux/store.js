302592730fcc9d8d5ac9a4451d31da8dd542b04d
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-27 19:35:42 +0200

    create root-saga to collect all sage and export it to use into store , import fetchcollectionStart and run it into store

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,6 +7,8 @@ import { fetchCollectionsStart } from './shop/shop.sagas'
 // this to persist the store
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
+import rootSaga from './root-saga'
+
 const sagaMiddleware = createSagaMiddleware()
 const middlewares = [sagaMiddleware];
 
@@ -14,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-sagaMiddleware.run(fetchCollectionsStart)
+sagaMiddleware.run(rootSaga)
     // this presistence version of our store
 export const persistor = persistStore(store);
 export default { store, persistor };
