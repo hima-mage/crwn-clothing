e3c713b718a362fa5847bfba1c578f45b27ff448
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-29 23:47:41 +0200

    require needed packages into server , and set env constant , and app settings

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-const express = require('express')
+const app = require('express')()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const path = require('path')
+const path = require('path');
+
+// env constants 
+const port = process.env.PORT || 5000
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+// app settings
+app.use(bodyParser.json()) // convert req body into json 
+app.use(bodyParser.urlencoded({ extended: true })) // url - escape the symbol and spaces
+app.use(cors()) // cross-origin to check the web-server origin
